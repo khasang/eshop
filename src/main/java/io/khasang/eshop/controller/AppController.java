31983f8cef02e5d234f67be8b51e4dc55fd577c0
Azon <azon.sk@gmail.com>
2017-12-25 20:38:33 +0300

    change sout

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -4,6 +4,9 @@ public class AppController {
     public static void main(String[] args) {
         System.out.println("Ok!");
         System.out.println("Program starting...");
+        System.out.println("User Jack");
+        System.out.println("User Vorobei");
+        System.out.println("User Cat");
         System.out.println("Done!");
     }
 }
