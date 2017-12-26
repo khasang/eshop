c38f810624d721132f6062e3cb63031ab762a4d3
Shmonderr <kloran@ya.ru>
2017-12-26 21:51:48 +0300

    more hi        2323121

--- src/main/java/io/khassang/ehop/NewController.java
+++ src/main/java/io/khassang/ehop/NewController.java
@@ -3,11 +3,9 @@ package io.khassang.ehop;
 public class NewController {
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("hihoi");
-        System.out.println("hihoi");
-        System.out.println("hihoi");
-        System.out.println("hi");
-        System.out.println("hi");
+        System.out.println("hi3213");
+        System.out.println("hi23" +
+                "123");
         System.out.println("hi");
         System.out.println("hi");
     }
