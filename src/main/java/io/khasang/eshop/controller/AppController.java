727dab43330e65af3dff01a2124043c5a743444d
JavaIMMB <electropo4ta2015@yandex.ru>
2017-12-26 12:37:38 +0300

    numbers program commit

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -2,6 +2,7 @@ package io.khasang.eshop.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("Ok!");
+        System.out.println("This program prints numbers from 1 to 100");
+        System.out.println("This is it");
     }
 }
