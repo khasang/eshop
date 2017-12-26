5a3783fea5de54d70c3d3402e62a65b0ecb6b521
Natali <nataliknpz@yandex.ru>
2017-12-26 19:18:27 +0300

    change sout

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -4,8 +4,11 @@ import static java.lang.System.*;
 
 public class AppController {
     public static void main(String[] args) {
-        out.println("OK!");
-        out.println("Program starting...");
-        out.println("Done!");
+        System.out.println("OK!");
+        System.out.println("Program starting...");
+        System.out.println("Done!");
+        System.out.println("User Jack");
+        System.out.println("User Vorobei");
+        System.out.println("User Cat" );
     }
 }
