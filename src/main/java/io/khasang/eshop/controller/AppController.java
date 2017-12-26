a709c2e361cf063e6304cc05326b75487bef1b1b
JavaIMMB <electropo4ta2015@yandex.ru>
2017-12-26 12:35:26 +0300

    alphabet commit

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -2,6 +2,7 @@ package io.khasang.eshop.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("Ok!");
+        System.out.println("This program prints alphabet");
+        System.out.println("This is it!");
     }
 }
