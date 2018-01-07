86e2a836c2c3771e1376dd93d734e1e42eee80de
ABeznosov <dorupr2@yandex.ru>
2018-01-07 22:50:52 +0300

    add second message

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -3,5 +3,6 @@ package io.khasang.eshop.controller;
 public class AppController {
     public static void main(String[] args) {
         System.out.println("Ok!");
+        System.out.println("Ok!");
     }
 }
