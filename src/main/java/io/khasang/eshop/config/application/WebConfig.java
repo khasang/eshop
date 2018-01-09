590c9db4ec56647d358d8e7afba427e20433434b
ABeznosov <dorupr2@yandex.ru>
2018-01-10 01:07:22 +0300

    add extends class WebConfig

--- src/main/java/io/khasang/eshop/config/application/WebConfig.java
+++ src/main/java/io/khasang/eshop/config/application/WebConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"io.khasang.eshop"})
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
