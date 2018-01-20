4c27c0e41debf764b9f2175fb45ba93e0a33e710
Azon <azon.sk@gmail.com>
2018-01-20 13:00:13 +0300

    add spring security with md

--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -3,9 +3,12 @@ package io.khasang.eshop.controller;
 import io.khasang.eshop.model.CatInterface;
 import io.khasang.eshop.model.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -29,6 +32,22 @@ public class AppController {
     @ResponseBody
     public String createTable(){
         return createTable.createTableStatus();
+    }
+
+    @RequestMapping("/admin/page")
+    public String getSecurePage(){
+        return "admin";
+    }
+
+    @RequestMapping("/user/**")
+    public String getUserPAge(){
+        return "user";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public String getEncryptedPassword(@PathVariable("password") String password) {
+        return new BCryptPasswordEncoder().encode(password);
     }
 
 }
