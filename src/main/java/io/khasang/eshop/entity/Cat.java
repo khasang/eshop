7f4e3fda82bc3736755b0e5cd6c672e9747088da
ABeznosov <dorupr2@yandex.ru>
2018-01-24 01:33:02 +0300

    add hibernate config

---
+++ src/main/java/io/khasang/eshop/entity/Cat.java
@@ -0,0 +1,39 @@
+package io.khasang.eshop.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cats")
+public class Cat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cat_id")
+    private long id;
+
+    private String name;
+    private String Description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
