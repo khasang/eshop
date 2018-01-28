6ae195a72100535391eebecf452ac5199b45e6a4
slashko <laser91@yandex.ru>
2018-01-28 21:31:22 +0300

    add Hibernate Config

--- src/main/java/io/khasang/eshop/config/application/WebConfig.java
+++ src/main/java/io/khasang/eshop/config/application/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
-//spring-dispatcher-servlet.xml
+//spring-dispatcher-servlet.xml аналах настройки в xml файла
 @Configuration
 //Вкл. WebMVS Spring FrameWork
 @EnableWebMvc
