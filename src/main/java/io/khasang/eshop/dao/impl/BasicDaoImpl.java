45d75a259cc44d41ab21a60d2ec01a9109a9e67a
Shmonderr <kloran@ya.ru>
2018-02-06 22:34:01 +0300

    05.02.2018 20.27

--- src/main/java/io/khasang/eshop/dao/impl/BasicDaoImpl.java
+++ src/main/java/io/khasang/eshop/dao/impl/BasicDaoImpl.java
@@ -45,6 +45,7 @@ public class BasicDaoImpl<T> implements BasicDao<T> {
     @Override
     public T add(T entity) {
         getSession().save(entity);
+        System.out.println("dasd");
         return entity;
     }
 
