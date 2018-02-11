7066ce51e8b5ada7352af2a9e666aedcbaba29a8
slashko <laser91@yandex.ru>
2018-02-12 00:13:23 +0300

    add associations @OneToMany and @OneToOne

--- src/main/java/io/khasang/eshop/entity/Cat.java
+++ src/main/java/io/khasang/eshop/entity/Cat.java
@@ -1,6 +1,8 @@
 package io.khasang.eshop.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Cats")
@@ -13,16 +15,14 @@ public class Cat {
     private String name;
     private String description;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<CatWoman> catWomanList = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Food food;
+
     @Transient
     private String additionnal;
-
-    public String getAdditionnal() {
-        return additionnal;
-    }
-
-    public void setAdditionnal(String aditionnal) {
-        this.additionnal = aditionnal;
-    }
 
     public long getId() {
         return id;
@@ -46,5 +46,29 @@ public class Cat {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<CatWoman> getCatWomanList() {
+        return catWomanList;
+    }
+
+    public void setCatWomanList(List<CatWoman> catWomanList) {
+        this.catWomanList = catWomanList;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public String getAdditionnal() {
+        return additionnal;
+    }
+
+    public void setAdditionnal(String additionnal) {
+        this.additionnal = additionnal;
     }
 }
