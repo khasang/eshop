d0270e29b3b527769d4918f5bd11a66a0a035fc1
slashko <laser91@yandex.ru>
2018-03-01 21:54:29 +0300

    add book.jsp, jquery.min.js, bootstrap.min.css

--- src/main/java/io/khasang/eshop/config/application/WebConfig.java
+++ src/main/java/io/khasang/eshop/config/application/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -26,4 +27,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         return viewResolver;
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css/");
+    }
 }
