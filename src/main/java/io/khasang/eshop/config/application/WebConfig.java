7a45a9588553f5b0697568cb824886af378c6896
AlexeyBeznosov <dorupr2@yandex.ru>
2018-03-02 18:56:25 +0300

    add book jsp

--- src/main/java/io/khasang/eshop/config/application/WebConfig.java
+++ src/main/java/io/khasang/eshop/config/application/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -19,5 +20,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setSuffix(".jsp");
         viewResolver.setContentType("text/html; charset=utf-8");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/views/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/views/css/");
     }
 }
---
+++ web/WEB-INF/views/book.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script src="js/jquery-3.3.1.js" type="text/javascript"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <script>
+        var service = "http://localhost:8080/book";
+        // var RestGet = function () {
+        //
+        // }
+    </script>
+</body>
+</html>
